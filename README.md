18dbfdf21e8866caf567425e537d48bcde418836
sigee <sigee15@gmail.com>
2017-08-08 20:34:28 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Maimaya_volgye
+MUD based on Nightmare 3.3 for MudOS v20.23
---
+++ win32/mud.bat
@@ -0,0 +1,7 @@
+# This improved batch file courtesy of Brodbane @ Eve
+goto start
+:start
+c:\nm3\win32\driver.exe c:\nm3\bin\mudos.cfg.win32
+# Add a '#' to the following line to prevent auto-restarts
+goto start
+:end
