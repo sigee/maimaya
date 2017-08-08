1eb3f4cd0f526ee9146ca96f6e8bf6119a419170
sigee <sigee15@gmail.com>
2017-08-08 22:14:32 +0200

    Translate workroom template

--- lib/std/obj/workroom.c
+++ lib/std/obj/workroom.c
@@ -1,23 +1,26 @@
-/*    /std/obj/workroom.c
+/*
+    /std/obj/workroom.c
     from Nightmare IV
     a standard workroom style object for new immortals
     created by Descartes of Borg 940316
 */
 
+#include <rooms.h>
 #include <std.h>
 
 inherit ROOM;
 
 void create() {
     ::create();
-    set_properties( ([ "light" : 2, "indoors" : 1 ]) );
-    set_short("a new workroom");
-    set_long(
-      "This is the grand workroom of a new immortal.  "
-      "Inventive new realms will originate here."
-    );
-    set_exits( ([ "square" : "/domains/standard/square" ]) );
-    set_items( ([ "workroom" : "Code is being created here." ]) );
-    set_smell("default", "It smells like a new immortal.");
-    set_listen("default", "The sounds of code creation vibrate the room.");
+    set_property("light", 2);
+    set_property("indoors", 1);
+    set_short("Az új halhatatlanok szobája");
+    set_long("Egy új halhatatlan dolgozik itt.");
+    set_exits( ([
+        "foter" : ROOM_START,
+        "hall" : ROOM_HALL
+    ]) );
+    set_items( ([ "targy" : "Ez egy tárgy a szobádban." ]));
+    set_smell("default", "Friss szag terjeng a levegõben.");
+    set_listen("default", "Hallod a billentyûk kopogását.");
 }
